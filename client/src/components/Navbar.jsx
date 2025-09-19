f66b3fa0bc847aa2888345558dd7529bbe3ae8e2
VictorOwen <victorowen113@icloud.com>
2025-09-19 19:08:09 +0300

    made changes to front-end component navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import "./Navbar.css";
 
 function Navbar() {
-    
+  const location = useLocation();
+  const [show, setShow] = useState(false);
+
+  const transitionNavbar = () => {
+    if(window.scrollY > 100){
+      setShow(true);
+    } else{
+      setShow(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => {
+      window.removeEventListener("scroll", transitionNavbar);
+    };
+  }, []);
+
   return (
     <div>
-        <div className='nav_contents'>
-          <div className='nav_elements'>
+        <div className={`navbar ${show ? "nav_show" : ""}`}>
+          <div className='nav_contents'>
             <ul>
-                <li> <link to= "/" className=''>Home</link></li>
-                <li> <link to= "/Hostels" className=''>Hostels</link></li>
-                <li> <Link to= "/Bookings" className=''>Bookings</Link></li>
-                <li> <Link to= "/Contact" className=''>Contact</Link></li>      
+                <li> <Link to= "/" className={location.pathname === "/" ? "active" : ""}>Home</Link></li>
+                <li> <Link to= "/Hostels" className={location.pathname === "/Hostels" ? "active" : ""}>Hostels</Link></li>
+                <li> <Link to= "/Bookings" className={location.pathname === "/Bookings" ? "active" : ""}>Bookings</Link></li>
+                <li> <Link to= "/ContactUs" className={location.pathname === "/ContactUs" ? "active" : ""}>Contact Us</Link></li>      
             </ul>
           </div>
-            <div className='nav_logo'>
+            <div className='nav_login'>
               <h1><Link to="/Login">Login</Link></h1>
               <button><Link to="/Register" className=''>Register</Link></button>
             </div>
