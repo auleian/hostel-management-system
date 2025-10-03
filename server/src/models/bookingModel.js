2bb43dcee41271347509fa493f43161e161095f0
WarrenG123 <warrenw639@gmail.com>
2025-10-03 10:31:01 +0300

    updated route names

--- server/src/models/bookingModel.js
+++ server/src/models/bookingModel.js
@@ -1,35 +1,35 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    phonenumber: {
-        type: String,
-        required: true,
-        unique: true
-    },
-     hostel: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Hostel",
-      required: true
-    },
-    roomType: {
-      type: String,
-      required: true,
-      enum: ["single", "double", "dorm"] // example room types
-    },
-    checkInDate: {
-        type: Date,
-        required: true
-    }
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phonenumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hostel: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hostel",
+    required: true,
+  },
+  roomType: {
+    type: String,
+    required: true,
+    enum: ["single", "double", "dorm"], // example room types
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
