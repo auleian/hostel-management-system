5ea323f965a4849e1d40f8320197d6728b46169b
WarrenG123 <warrenw639@gmail.com>
2025-10-10 10:51:18 +0300

    removed the redundant details in booking schema

--- server/src/models/bookingModel.js
+++ server/src/models/bookingModel.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
     room: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Room",
