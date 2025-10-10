784420716cdaee8fdfb9fdea02ef0375ad273647
auleian <aule.ian@students.mak.ac.ug>
2025-10-10 14:58:09 +0300

    Created the registration and login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import RegisterPage from './Pages/Registration.jsx'
+import LoginPage from './Pages/LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RegisterPage />
+    <LoginPage />
     <App />
   </StrictMode>,
 )
