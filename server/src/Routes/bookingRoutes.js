0919fb9acc96f348cea326dc3c5f745455bd733c
auleian <aule.ian@students.mak.ac.ug>
2025-11-10 13:50:54 +0300

    added the route for updating booking status

--- server/src/Routes/bookingRoutes.js
+++ server/src/Routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createBooking, getAllBookings } from "../controllers/bookingController.js";
+import { createBooking, getAllBookings, updateBookingStatus } from "../controllers/bookingController.js";
 
 const router = Router();
 
 router.post("/", createBooking);
 router.get("/", getAllBookings);
+router.patch("/:id", updateBookingStatus);
 
 export default router;
